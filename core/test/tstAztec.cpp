aed60a40d19d43e210319d76ac06c85cac1464ae
Stuart Slattery <sslattery617@gmail.com>
2012-04-13 18:11:51 -0500

    Setup and solved an epetra linear problem with the aztec gmres solver

--- core/test/tstAztec.cpp
+++ core/test/tstAztec.cpp
@@ -15,6 +15,7 @@
 #include <vector>
 #include <cmath>
 #include <sstream>
+#include <ostream>
 
 #include <Teuchos_UnitTestHarness.hpp>
 
@@ -22,6 +23,9 @@
 #include <Epetra_Map.h>
 #include <Epetra_Vector.h>
 #include <Epetra_CrsMatrix.h>
+#include <Epetra_LinearProblem.h>
+
+#include <AztecOO.h>
 
 //---------------------------------------------------------------------------//
 // TESTS
@@ -29,53 +33,46 @@
 
 TEUCHOS_UNIT_TEST( AztecOO, gmres_test)
 {
-    int error = 0;
+    int problem_size = 1000;
 
-    std::vector<double> x_vector( 3, 0.0 );
     Epetra_SerialComm comm;
-    Epetra_Map map( 3, 0, comm );
+    Epetra_Map map( problem_size, 0, comm );
+
+    std::vector<double> x_vector( problem_size );
     Epetra_Vector x( View, map, &x_vector[0] );
 
-    std::vector<double> b_vector( 3 );
-    b_vector[0] = 10.0;
-    b_vector[1] = 1.0;
-    b_vector[2] = 1.0;
+    std::vector<double> b_vector( problem_size, 0.4 );
     Epetra_Vector b( View, map, &b_vector[0] );
 
-    std::vector<double> A_matrix( 9 );
-    A_matrix[0] = 1.0;
-    A_matrix[1] = -0.3;
-    A_matrix[2] = -0.4;
-    A_matrix[3] = -0.2;
-    A_matrix[4] = 1.0;
-    A_matrix[5] = -0.4;
-    A_matrix[6] = -0.5;
-    A_matrix[7] = -0.4;
-    A_matrix[8] = 1.0;
-
-    std::vector<int> A_indices( 9 );
-    for ( int i = 0; i < 2; ++i )
+    Epetra_CrsMatrix A( Copy, map, problem_size );
+    double lower_diag = -1.0;
+    double diag = 2.0;
+    double upper_diag = -1.0;
+    int global_row = 0;
+    int lower_row = 0;
+    int upper_row = 0;
+    for ( int i = 0; i < problem_size; ++i )
     {
-	A_indices[i] = i;
-	A_indices[i+3] = i;
-	A_indices[i+6] = i;
+	global_row = i;
+	lower_row = i-1;
+	upper_row = i+1;
+	if ( lower_row > -1 )
+	{
+	    A.InsertGlobalValues( global_row, 1, &lower_diag, &lower_row );
+	}
+	A.InsertGlobalValues( global_row, 1, &diag, &global_row );
+	if ( upper_row < problem_size )
+	{
+	    A.InsertGlobalValues( global_row, 1, &upper_diag, &upper_row );
+	}
     }
+    A.FillComplete();
 
-    std::vector<int> entries_per_row( 3, 3 );
-    Epetra_CrsMatrix A( View, map, &entries_per_row[0] );
-    A.InsertGlobalValues( 0, 1, &A_matrix[0], &A_indices[0] );
-    A.InsertGlobalValues( 0, 1, &A_matrix[1], &A_indices[1] );
-    A.InsertGlobalValues( 0, 1, &A_matrix[2], &A_indices[2] );
-    A.InsertGlobalValues( 1, 1, &A_matrix[3], &A_indices[3] );
-    A.InsertGlobalValues( 1, 1, &A_matrix[4], &A_indices[4] );
-    A.InsertGlobalValues( 1, 1, &A_matrix[5], &A_indices[5] );
-    A.InsertGlobalValues( 2, 1, &A_matrix[6], &A_indices[6] );
-    A.InsertGlobalValues( 2, 1, &A_matrix[7], &A_indices[7] );
-    A.InsertGlobalValues( 2, 1, &A_matrix[8], &A_indices[8] );
-    error = A.FillComplete();
-    TEST_ASSERT( error > 0 );
+    Epetra_LinearProblem linear_problem( &A, &x, &b );
 
-    
+    AztecOO aztec_solver( linear_problem );
+    aztec_solver.SetAztecOption( AZ_solver, AZ_gmres );
+    aztec_solver.Iterate( 100, 1.0e-8 );
 }
 
 //---------------------------------------------------------------------------//
